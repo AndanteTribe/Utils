3943928b6b40b573f73d7446800fd2ecec7aea2d
Masatake Iwasaki <78918084+kochounoyume@users.noreply.github.com>
2025-05-03 02:17:29 +0900

    Add EditorTest

---
+++ src/Utils.Unity/Assets/Tests/Editor/EditorTest.cs
@@ -0,0 +1,22 @@
+﻿#nullable enable
+
+using AndanteTribe.Utils.Editor;
+using NUnit.Framework;
+using UnityEditor;
+
+namespace AndanteTribe.Utils.Tests.Editor
+{
+    public class EditorTest
+    {
+        [Test]
+        [TestCase(BuildTarget.StandaloneWindows)]
+        [TestCase(BuildTarget.StandaloneWindows64)]
+        [TestCase(BuildTarget.StandaloneOSX)]
+        [TestCase(BuildTarget.StandaloneLinux64)]
+        [TestCase(BuildTarget.Android)]
+        [TestCase(BuildTarget.iOS)]
+        [TestCase(BuildTarget.WebGL)]
+        public void CompileCheckTest(BuildTarget platform)
+            => Assert.That(TestUtils.SuccessCompile(platform), Is.True);
+    }
+}
