eeb5c0112576da2c80e174d9b4b209623de314b7
Masatake Iwasaki <78918084+kochounoyume@users.noreply.github.com>
2025-12-05 02:46:11 +0900

    feat: add SkewMeshEffect

---
+++ src/Utils.Unity/Packages/jp.andantetribe.utils/Runtime/UGUI/SkewMeshEffect.cs
@@ -0,0 +1,43 @@
+#if ENABLE_UGUI
+#nullable enable
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AndanteTribe.Utils.Unity.UGUI
+{
+    /// <summary>
+    /// UGUIのメッシュを斜めに変形するエフェクト.
+    /// </summary>
+    /// <remarks>
+    /// <see cref="Image"/>のゲームオブジェクトにアタッチすると、見た目も切り口も斜めなゲージなどを表現できる.
+    /// </remarks>
+    [RequireComponent(typeof(Graphic))]
+    public class SkewMeshEffect : BaseMeshEffect
+    {
+        [SerializeField]
+        private Vector2 _skew = new Vector2(0.5f, 0f);
+
+        /// <inheritdoc />
+        public override void ModifyMesh(VertexHelper helper)
+        {
+            if (!IsActive())
+            {
+                return;
+            }
+
+            var vertex = new UIVertex();
+            for (var i = 0; i < helper.currentVertCount; i++)
+            {
+                helper.PopulateUIVertex(ref vertex, i);
+                var pos = vertex.position;
+                pos.x += _skew.x * pos.y;
+                pos.y += _skew.y * pos.x;
+                vertex.position = pos;
+                helper.SetUIVertex(vertex, i);
+            }
+        }
+    }
+}
+
+#endif
