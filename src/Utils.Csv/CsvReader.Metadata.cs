cef32a649d6fc2f90158917e834d4323e3e81e7d
Masatake Iwasaki <78918084+kochounoyume@users.noreply.github.com>
2026-02-23 16:06:28 +0900

    refactor: Replace TypeCode checks with RuntimeTypeHandle for improved type handling in CsvReader

--- src/Utils.Csv/CsvReader.Metadata.cs
+++ src/Utils.Csv/CsvReader.Metadata.cs
@@ -6,6 +6,28 @@ namespace AndanteTribe.Utils.Csv;
 
 public partial class CsvReader
 {
+    private readonly RuntimeTypeHandle _boolTypeHandle = typeof(bool).TypeHandle;
+    private readonly RuntimeTypeHandle _charTypeHandle = typeof(char).TypeHandle;
+    private readonly RuntimeTypeHandle _sbyteTypeHandle = typeof(sbyte).TypeHandle;
+    private readonly RuntimeTypeHandle _byteTypeHandle = typeof(byte).TypeHandle;
+    private readonly RuntimeTypeHandle _shortTypeHandle = typeof(short).TypeHandle;
+    private readonly RuntimeTypeHandle _ushortTypeHandle = typeof(ushort).TypeHandle;
+    private readonly RuntimeTypeHandle _intTypeHandle = typeof(int).TypeHandle;
+    private readonly RuntimeTypeHandle _uintTypeHandle = typeof(uint).TypeHandle;
+    private readonly RuntimeTypeHandle _longTypeHandle = typeof(long).TypeHandle;
+    private readonly RuntimeTypeHandle _ulongTypeHandle = typeof(ulong).TypeHandle;
+    private readonly RuntimeTypeHandle _floatTypeHandle = typeof(float).TypeHandle;
+    private readonly RuntimeTypeHandle _doubleTypeHandle = typeof(double).TypeHandle;
+    private readonly RuntimeTypeHandle _decimalTypeHandle = typeof(decimal).TypeHandle;
+    private readonly RuntimeTypeHandle _dateTimeTypeHandle = typeof(DateTime).TypeHandle;
+    private readonly RuntimeTypeHandle _stringTypeHandle = typeof(string).TypeHandle;
+    private readonly RuntimeTypeHandle _dateTimeOffsetTypeHandle = typeof(DateTimeOffset).TypeHandle;
+    private readonly RuntimeTypeHandle _timeSpanTypeHandle = typeof(TimeSpan).TypeHandle;
+    private readonly RuntimeTypeHandle _guidTypeHandle = typeof(Guid).TypeHandle;
+    private readonly RuntimeTypeHandle _uriTypeHandle = typeof(Uri).TypeHandle;
+    private readonly RuntimeTypeHandle _versionTypeHandle = typeof(Version).TypeHandle;
+    private readonly RuntimeTypeHandle _enumTypeHandle = typeof(Enum).TypeHandle;
+
     /// <summary>
     /// 型情報から値を読み込む.
     /// </summary>
@@ -29,30 +51,30 @@ public partial class CsvReader
             type = type.GenericTypeArguments[0];
         }
 
-        return Type.GetTypeCode(type) switch
+        return type.TypeHandle switch
         {
             // 列挙体の型は基底型(デフォルトでint型)で出力してしまうので、最優先で処理する.
-            _ when type.IsEnum => ReadEnumValue(type),
-            TypeCode.Boolean => ReadBool(),
-            TypeCode.Char => ReadChar(),
-            TypeCode.SByte => ReadSbyte(),
-            TypeCode.Byte => ReadByte(),
-            TypeCode.Int16 => ReadShort(),
-            TypeCode.UInt16 => ReadUshort(),
-            TypeCode.Int32 => ReadInt(),
-            TypeCode.UInt32 => ReadUint(),
-            TypeCode.Int64 => ReadLong(),
-            TypeCode.UInt64 => ReadUlong(),
-            TypeCode.Single => ReadFloat(),
-            TypeCode.Double => ReadDouble(),
-            TypeCode.Decimal => ReadDecimal(),
-            TypeCode.DateTime => ReadDateTime(),
-            TypeCode.String => ReadString(),
-            _ when type == typeof(DateTimeOffset) => ReadDateTimeOffset(),
-            _ when type == typeof(TimeSpan) => ReadTimeSpan(),
-            _ when type == typeof(Guid) => ReadGuid(),
-            _ when type == typeof(Uri) => ReadUri(),
-            _ when type == typeof(Version) => ReadVersion(),
+            _ when type.BaseType?.TypeHandle.Equals(_enumTypeHandle) == true => ReadEnumValue(type),
+            _ when type.TypeHandle.Equals(_boolTypeHandle) => ReadBool(),
+            _ when type.TypeHandle.Equals(_charTypeHandle) => ReadChar(),
+            _ when type.TypeHandle.Equals(_sbyteTypeHandle) => ReadSbyte(),
+            _ when type.TypeHandle.Equals(_byteTypeHandle) => ReadByte(),
+            _ when type.TypeHandle.Equals(_shortTypeHandle) => ReadShort(),
+            _ when type.TypeHandle.Equals(_ushortTypeHandle) => ReadUshort(),
+            _ when type.TypeHandle.Equals(_intTypeHandle) => ReadInt(),
+            _ when type.TypeHandle.Equals(_uintTypeHandle) => ReadUint(),
+            _ when type.TypeHandle.Equals(_longTypeHandle) => ReadLong(),
+            _ when type.TypeHandle.Equals(_ulongTypeHandle) => ReadUlong(),
+            _ when type.TypeHandle.Equals(_floatTypeHandle) => ReadFloat(),
+            _ when type.TypeHandle.Equals(_doubleTypeHandle) => ReadDouble(),
+            _ when type.TypeHandle.Equals(_decimalTypeHandle) => ReadDecimal(),
+            _ when type.TypeHandle.Equals(_dateTimeTypeHandle) => ReadDateTime(),
+            _ when type.TypeHandle.Equals(_stringTypeHandle) => ReadString(),
+            _ when type.TypeHandle.Equals(_dateTimeOffsetTypeHandle) => ReadDateTimeOffset(),
+            _ when type.TypeHandle.Equals(_timeSpanTypeHandle) => ReadTimeSpan(),
+            _ when type.TypeHandle.Equals(_guidTypeHandle) => ReadGuid(),
+            _ when type.TypeHandle.Equals(_uriTypeHandle) => ReadUri(),
+            _ when type.TypeHandle.Equals(_versionTypeHandle) => ReadVersion(),
             _ => throw new NotSupportedException($"The type '{type.FullName}' is not supported.")
         };
     }
